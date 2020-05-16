90357db615cd78b4a3a11e8e45cd8c60cd45de57
SK <59910993+TikuisChan@users.noreply.github.com>
2020-05-16 23:31:40 +0800

    Update insert_nth.py
    simplified version

--- linked list/insert_nth.py
+++ linked list/insert_nth.py
@@ -1,28 +1,17 @@
 class Node(object):
-    def __init__(self, data):
+    def __init__(self, data, next=None):
         self.data = data
-        self.next = None
+        self.next = next
     
 def insert_nth(head, index, data):
     if index < 0:
         raise "Index ValueError"
-    if head is None:
-        return Node(data)
-    current_index = 0
+    if head is None or index == 0:
+        return Node(data, head)
     n = head
-    while n:
-        if index == 0:
-            m = Node(data)
-            m.next = n
-            return m
-        if current_index == 0:
-            ans = n
-        if current_index == index - 1:
-            m = n.next
-            n.next = Node(data)
-            n = n.next
-            n.next = m
-            return ans
-        current_index += 1
+    ans = n
+    while index > 1:
+        index -= 1
         n = n.next
-    raise 'ValueError'
+    n.next = Node(data, n.next)
+    return ans
